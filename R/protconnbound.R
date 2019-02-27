a11d463bee6940cc2aab27fb55de2964991146d1
Luis Carrasco <lcarrasco@bufo.nimbios.org>
2019-02-27 09:37:25 -0500

    Create script

---
+++ R/protconnbound.R
@@ -0,0 +1,22 @@
+# protconnbound.R
+# on 26 February 2019
+# by Luis
+# calculates connectivity at the country level for PACC project,
+# based on ProtConn-Bound from Saura et al. 2018.
+#################################################################################################
+
+library(rgdal)
+library(maptools)
+library(rgeos)
+library(sf)
+library(MetaLandSim)
+
+
+# 0. FUNCTIONS
+
+
+
+# 1. READ DATA
+
+# 2. MAIN ROUTINE
+
