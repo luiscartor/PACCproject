bf35f45808848a07495a8354d7fee85299b99a2a
Carrasco Tornero <lcarrasc@utk.edu>
2019-04-01 12:58:54 -0400

    conefor in win

--- R/protconnbound.R
+++ R/protconnbound.R
@@ -15,32 +15,38 @@ library(raster)
 
 # 0. FUNCTIONS
 # PCfun calculates PC using conefor software
-coneforpath <- paste(getwd(),"/coneforLinux64",sep="")
+coneforpath <- paste(getwd(),"/coneforWin64",sep="")
 
-PCfun <- function(attfile, disfile){
+#system("/home/lcarrasco/Documents/research/protectedareas/PACCproject/coneforLinux64")
+system("C:\\Users\\lcarrasc\\Documents\\research\\protectedareas\\PACCproject\\coneforWin64.exe")
+
+PCfun <- function(attfile, disfile, countrycode){
   
-  #system("/home/lcarrasco/Documents/research/protectedareas/PACCproject/coneforLinux64")
-  
-  command <- paste(coneforpath,"-nodeFile", attfile,"-conFile",disfile, "-t dist all -confProb 200 0.5 -PC onlyoverall", collapse = ' ')
-  system(command)
+  command <- paste(coneforpath,"-nodeFile", attfile,"-conFile",disfile, "-t dist notall -confProb 10000 0.5 -PC onlyoverall -prefix", countrycode, collapse = ' ')
+  shell(command)
   
 }
 
 # 1. INPUTS
-INattfolder <- "/home/lcarrasco/Documents/research/protectedareas/connectivity/coneforfiles_r/attributes/"
-INdisfolder <- "/home/lcarrasco/Documents/research/protectedareas/connectivity/coneforfiles_r/distances/"
-INattfolder <- "/home/lcarrasco/Documents/research/protectedareas/connectivity/coneforfiles_qgis/attribute/"
-INdisfolder <- "/home/lcarrasco/Documents/research/protectedareas/connectivity/coneforfiles_qgis/distances/"
+#INattfolder <- "/home/lcarrasco/Documents/research/protectedareas/connectivity/coneforfiles_r/attributes/"
+#INdisfolder <- "/home/lcarrasco/Documents/research/protectedareas/connectivity/coneforfiles_r/distances/"
+#INattfolder <- "/home/lcarrasco/Documents/research/protectedareas/connectivity/coneforfiles_qgis/attribute/"
+#INdisfolder <- "/home/lcarrasco/Documents/research/protectedareas/connectivity/coneforfiles_qgis/distances/"
+
+INfolder <- "C:\\Users\\lcarrasc\\Documents\\research\\protectedareas\\analysis\\connectivity\\conefor_inputs_arcgis_fortest\\"
 
 
 
 # 2. READ DATA
 #gadm <- readOGR(INgadmfolder, 'gadm36_0_simplify')
-gadm <- shapefile('/home/lcarrasco/Documents/research/protectedareas/data/GADM/gadm36_0_simplify')
+#gadm <- shapefile('/home/lcarrasco/Documents/research/protectedareas/data/GADM/gadm36_0_simplify')
+gadm <- shapefile('C:\\Users\\lcarrasc\\Documents\\research\\chinese_infrastructures\\datasets\\GADM\\gadm36_0_simplify')
 
-pas <- shapefile('/home/lcarrasco/Documents/research/protectedareas/data/WDPA/WDPA_2011on_final')
-pastilldate <- shapefile('/home/lcarrasco/Documents/research/protectedareas/data/WDPA/WDPA_till2010_final')
+#pas <- shapefile('/home/lcarrasco/Documents/research/protectedareas/data/WDPA/WDPA_2011on_final')
+pas <- shapefile('C:\\Users\\lcarrasc\\Documents\\research\\protectedareas\\data\\WDPA_Global\\WDPA_2011on_final')
 
+#pastilldate <- shapefile('/home/lcarrasco/Documents/research/protectedareas/data/WDPA/WDPA_till2010_final')
+pastilldate <- shapefile('C:\\Users\\lcarrasc\\Documents\\research\\protectedareas\\data\\WDPA_Global\\WDPA_till2010_final')
 
 
 
@@ -55,21 +61,27 @@ for (c in countrynames){
   if(file.exists(paste(INattfolder,c,"firstnetworkatt.txt",sep = ""))){
   
     # 3.1 Read network conefor files
-    attfile1st <- paste(INattfolder,c,"firstnetworka.txt",sep = "")
-    disfile1st <- paste(INdisfolder,c,"firstnetworkd.txt",sep = "")
-    attfile2nd <- paste(INattfolder,c,"secondnetworkatt.txt",sep = "")
-    disfile2nd <- paste(INdisfolder,c,"secondnetworkdis.txt",sep = "")
+    #attfile1st <- paste(INattfolder,c,"firstnetworka.txt",sep = "")
+    #disfile1st <- paste(INdisfolder,c,"firstnetworkd.txt",sep = "")
+    #attfile2nd <- paste(INattfolder,c,"secondnetworkatt.txt",sep = "")
+    #disfile2nd <- paste(INdisfolder,c,"secondnetworkdis.txt",sep = "")
+    
+    attfile1st <- paste(INfolder,"nodes_",c,"firstnetwork.txt",sep = "",collapse = "")
+    disfile1st <- paste(INfolder,"distances_",c,"firstnetwork.txt",sep = "",collapse = "")
+    attfile2nd <- paste(INfolder,"nodes_",c,"secondnetwork.txt",sep = "",collapse = "")
+    disfile2nd <- paste(INfolder,"distances_",c,"secondnetwork.txt",sep = "",collapse = "")
     
     # 3.2 Calculates PC (probability of connectivity) index with conefor for both networks
     
-    PC1st <- PCfun(attfile1st,disfile1st)
-    PC2nd <- PCfun(attfile2nd,disfile2nd)
+    PC1st <- PCfun(attfile1st,disfile1st,c)
+    PC2nd <- PCfun(attfile2nd,disfile2nd,c)
     
     # 3.3 Calculates ProtUnconn[Design] (Saura 2018 Appendix B.3)
     # Needs country area
     countryarea <- area(subset(gadm, GID_0==c))
     
     protunconndes <- (100*(PC1st/countryarea)) - (100*(PC2nd/countryarea))
+    protunconndes <- (100*(26000000/countryarea)) - (100*(PC2nd/countryarea))
     
     # 3.4 Calculates ProtConnBound; ProtConnBound=Prot−ProtUnconn[Design]
     # Needs the total protected area of considered dataset
@@ -90,3 +102,5 @@ colnames(protconnboundtable) <- c("country","protconnbound")
 
 #write.table(protconnboundtable, "/home/lcarrasco/Documents/research/protectedareas/connectivity/results/protconnboundtable.txt",
  #           row.names = F, col.names = F)
+
+
