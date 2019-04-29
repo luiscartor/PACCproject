cad1537e9772baf57b11fe9e9f48292cae7e2b95
Luis Carrasco <lcarrasco@bufo.nimbios.org>
2019-04-29 17:27:49 -0400

    extractnetworks_netsonly cleaned and commented

--- R/extractnetworks_netsonly.R
+++ R/extractnetworks_netsonly.R
@@ -1,9 +1,8 @@
 # extractnetworks.R
 # on 1st March 2019
 # by Luis
-# extract PAs networks and creates shapefiles to ingest to conefor in QGIS
-##################################################################################################################
-
+# extract PAs networks and creates shapefiles to ingest to conefor input files plugin
+####################################################################################################################
 
 library(rgdal)
 library(maptools)
@@ -11,97 +10,73 @@ library(rgeos)
 library(sf)
 library(raster)
 
+
 # 1. INPUTS
-# Read country boundaries data (each piece of land is a different feature)
-#INgadmfolder <- '/home/lcarrasco/Documents/research/protectedareas/data/GADM/'
-#INpasfolder <- '/home/lcarrasco/Documents/research/protectedareas/data/WDPA/'
 
+# Protected areas file (for all years: WDPA_cleaned_all_final, or until 2010: WDPA_cleaned_till2010_final)
 INpasfile <- '/home/lcarrasco/Documents/research/protectedareas/data/WDPA_under1km/WDPA_cleaned_all_final'
+# Country boundaries
+INgadmfile <- '/home/lcarrasco/Documents/research/protectedareas/data/GADM/gadm36_0_simplify'
+# Country boundaries where each piece of land is a different feature
+INgadmmultifile <- '/home/lcarrasco/Documents/research/protectedareas/data/GADM/gadm36_0_multipart'
+# Country boundaries with a 500km buffer
+INgadmbufferfile <- '/home/lcarrasco/Documents/research/protectedareas/data/GADM/gadm36_0_500kmbuffer'
 
-
+# Output folder for the networks
 OUTnetworkfolder <- '/home/lcarrasco/Documents/research/protectedareas/connectivity/networks_all_500km_simp100/'
 
 
 # 2. READ DATA
-# Read country boundaries data (each piece of land is a different feature)
-gadm <- shapefile('/home/lcarrasco/Documents/research/protectedareas/data/GADM/gadm36_0_simplify')
-gadmmulti <-shapefile('/home/lcarrasco/Documents/research/protectedareas/data/GADM/gadm36_0_multipart')
-gadmbuffer <- shapefile('/home/lcarrasco/Documents/research/protectedareas/data/GADM/gadm36_0_500kmbuffer')
 
-
+# Reads PAs data
 pas <- shapefile(INpasfile)
+# Reads country boundaries data 
+gadm <- shapefile(INgadmfile)
+gadmmulti <-shapefile(INgadmmultifile)
+gadmbuffer <- shapefile(INgadmbufferfile)
 
 
 # 3. MAIN ROUTINE
 
+# 3.0.1 Calculates area of each PA polygon and delete polygons <1km2
 pas@data$area <- area(pas)
 pas <- subset(pas,area>=1000000)
 
-# 3.0 Obtain number of countries and create loop
+# 3.0.2 Obtains number of countries and create loop
 countrynames <- unique(gadm@data$GID_0)
-
 
 for (c in countrynames){
   
-  #Run only for countries with PAs and avoid ANT (antartica)
+  #Run only for countries with existing PAs and avoid ANT (antartica)
   if(any(pas@data$ISO3==c & c != "ATA")){
   
     print(paste("Extracting networks for ",c,sep=""))
     
-    # 3.1 Create First network dataset (ProtUnconnDesign; Saura 2018, appendix)
+    # 3.1 Creates "First Network" dataset (ProtUnconnDesign; Saura 2018, appendix)
     # The first network uses all PAs within the country, with an attribute equal to area, all transboundary PAs,
     # with attribute equal zero, and all the land portions of the country.
     
-    # 3.1.1 Extracts country pas and add area
+    # 3.1.1 Extracts country pas and add area as attribute
     counpas <- subset(pas, ISO3==c)
-    # Dissolve polygons avoiding overlap
-    #counpas <- aggregate(counpas,by="ISO3",dissolve=TRUE)
-    # Disaggregate so that we have multiple features
-    #counpas <- disaggregate(counpas)
     # Adds attribute (area) column
     counpas@data$attribute <- counpas@data$area
-    # Delete areas < 1km2, in case some small polygons were created after dissolving/cleaning
-    #counpas <- subset(counpas,attribute>=1000000)
-    # Skip country (leave loop) if no areas > 1km
-    #if(length(counpas@polygons)==0){
-    #  print(paste("No PAs > 1km2 for ",c,sep=""))
-    #  next
-    #}
-    
-    # Clip to country to avoid sea areas
-    #counlimits <- subset(gadm, GID_0==c)
-    #counpas2 <- gIntersection(counpas, counlimits, byid=TRUE)
-    
-    
-    # Leaves only attribute column
+    # Leaves attribute column only
     counpas <- counpas[,-(1:7)]
     
-    # 3.1.2 Extract transboundary pas and add 0 attribute
+    # 3.1.2 Extracts transboundary PAs and adds 0 attribute
+    # Selects buffer
     countrans <- subset(gadmbuffer, GID_0==c)
-
-
-    # But excluding the country PAs
+    # Selects PAs within the buffer, but excluding the within-country PAs
     pasoutside <- subset(pas, ISO3!=c)
     
-    
-    # Select PAs included in countrans
+    # Selects PAs included in countrans
     # Here we use the overlay function to detect the polygons that overlay with 500km boundary (gives pol index)
     paswithin <- over(pasoutside,geometry(countrans))
-    
     # Subsets to the polygons within the boundary
     transbound <- pasoutside[which(!is.na(paswithin)),]
     
-    # Dissolve polygons avoiding overlap
-    #transbound <- aggregate(transbound,by="ISO3",dissolve=TRUE)
-    # Disaggregate so that we have multiple features
-    #transbound <- disaggregate(transbound)
-    
-    
     # In case there are no countries around, we still need transbound (with 0 polygons)
     if(length(transbound@polygons)!=0){
-      # We want to avoid areas < 1sqr km
-      #transbound@data$area <- area(transbound)
-      #transbound <- subset(transbound,area>=1000000)
       # Adds attribute column (equal to zero)
       transbound@data$attribute <- rep(0,nrow(transbound@data))
       # Leaves only attribute column
@@ -120,8 +95,8 @@ for (c in countrynames){
     firstnet <- rbind(counpas,transbound,counland, makeUniqueIDs = TRUE)
     firstnet@data$ID <- seq(1,nrow(firstnet@data))
     
-    # 3.1.5 Calculate distance between edges of polygons
-    # 3.1.5.1 Reproject to azimuthal equidistant proj to calculate distances
+    # 3.1.5 Calculates distance between edges of polygons
+    # 3.1.5.1 Reprojects to azimuthal equidistant proj to calculate distances
     # Calculate centroid of country
     centroid <- gCentroid(counland)
     # crs with country centroid as map center
@@ -130,14 +105,14 @@ for (c in countrynames){
     firstnet_laea <- spTransform(firstnet, CRS(crs_coun))
     
     
-    # 3.1.X Writes  shapefile to disk
+    # 3.1.6 Writes  shapefile to disk
     writeOGR(obj=firstnet_laea, dsn=OUTnetworkfolder, layer=paste(c,"firstnetwork",sep=""), driver="ESRI Shapefile",
            overwrite_layer = TRUE)
     # !!! Writting gives a warning, but is an unnecessary one (known issue): https://trac.osgeo.org/gdal/ticket/6803
     
    
     
-    # 3.2 Create Second network dataset.
+    # 3.2 Creates "Second Network" dataset: (ProtUnconnDesign; Saura 2018, appendix)
     # The second network uses only the Pas within the country, with attribute equal to area, and the transboundary
     # PAs with attribute equal zero.
     
@@ -150,7 +125,6 @@ for (c in countrynames){
     # 3.2.X Writes to shapefile to disk
     writeOGR(obj=secondnet_laea, dsn=OUTnetworkfolder, layer=paste(c,"secondnetwork",sep=""), driver="ESRI Shapefile",
            overwrite_layer = TRUE)
-    
     
   }
 }
